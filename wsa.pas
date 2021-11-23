148023ef32edbd450a62b84fff4fd9035b6783b7
vhanla <vhanla@gmail.com>
2021-11-22 23:12:51 -0500

    Refactoring to wsa.pas

--- wsa.pas
+++ wsa.pas
@@ -3,7 +3,7 @@ unit wsa;
 interface
 
 uses
-  Winapi.Windows, Generics.Collections, System.Classes;
+  Winapi.Windows, Generics.Collections, System.Classes, Vcl.ImageCollection;
 
 type
   PAPK = ^TAPK;
@@ -34,6 +34,7 @@ type
   TWSA = class
   private
     FInstalledApps: TAPKList;
+    FImageCollection: TImageCollection;
     FInstallationPath: string;
     FAppUserModelId: string;
     FVersion: string;
@@ -75,15 +76,21 @@ type
 
 implementation
 
+uses
+  Winapi.ShlObj, Winapi.ActiveX, Winapi.KnownFolders, Winapi.ShellAPI,
+  Winapi.PropKey, Vcl.Graphics, System.SysUtils;
+
 { TWSA }
 
 constructor TWSA.Create;
 begin
   FInstalledApps := TAPKList.Create;
+  FImageCollection := TImageCollection.Create(nil);
 end;
 
 destructor TWSA.Destroy;
 begin
+  FImageCollection.Free;
   FInstalledApps.Free;
   inherited;
 end;
@@ -139,8 +146,131 @@ begin
 end;
 
 procedure TWSA.UpdateInstalledAPKList;
-begin
+var
+  io: IKnownFolderManager;
+  count: Cardinal;
+  a: PGUIDList;
+  knownfoldernative: IKnownFolder;
+  hr: HRESULT;
+  oguid: TGUID;
+  si: IShellItem2;
+  sfgao: Cardinal;
+  isFileSystem: Boolean;
+  fd: TKnownFolderDefinition;
+  Item: TStrings;
+  fPath: array[0..1024] of Char;
 
+  pidControl: PItemIDList;
+  psfDesktop, dt: IShellFolder;
+  psfControl: IShellFolder;
+  pc: IShellFolder2;
+  pEnumList: IEnumIDList;
+  pidChild: PItemIDList;
+  pidAbsolute: PItemIDList;
+  celtFetched: ULONG;
+  fileInfo: SHFILEINFOW;
+begin
+  CoInitialize(nil);
+
+  // Clear content
+  FImageCollection.Images.Clear;
+  FInstalledApps.Clear;
+
+  CoCreateInstance(CLSID_KnownFolderManager, nil, CLSCTX_ALL, IID_IKnownFolderManager, io);
+  if Assigned(io) then
+  begin
+  //FOLDERID_AppsFolder 1e87508d-89c2-42f0-8a7e-645a0f50ca58
+    hr := io.GetFolder(FOLDERID_AppsFolder, knownfoldernative);
+    if hr = S_OK then
+    begin
+      hr := knownfoldernative.GetShellItem(0, IID_IShellItem2, si);
+      if Succeeded(hr) then
+      begin
+        if si <> nil then
+        begin
+          si.GetAttributes(SFGAO_FILESYSTEM, sfgao);
+          isFileSystem := (sfgao and SFGAO_FILESYSTEM) <> 0;
+
+          if not isFileSystem then
+          begin
+            knownfoldernative.GetFolderDefinition(fd);
+            if SHGetDesktopFolder(psfDesktop) = S_OK then
+              if SHGetDesktopFolder(dt) = S_OK then
+                if knownfoldernative.GetIDList(KF_FLAG_DEFAULT, pidControl) = S_OK then
+                  if psfDesktop.BindToObject(pidControl, nil, IID_IShellFolder, psfControl) = S_OK then
+                    if psfControl.EnumObjects(0, SHCONTF_NONFOLDERS or SHCONTF_INCLUDEHIDDEN, pEnumList) S_OK then
+                    begin
+                      while pEnumList.Next(1, pidChild, celtFetched) = 0 do
+                      begin
+                        pidAbsolute := ILCombine(pidControl, pidChild);
+                        if dt.BindToObject(pidControl, nil, IID_IShellFolder2, Pointer(pc)) = S_OK then
+                        begin
+                          var sa, lnk, flnk: OleVariant;
+//                          var cs: SHCOLUMNID;
+//                          cs.fmtid := StringToGUID('{9F4C2855-9F79-4B39-A8D0-E1D42DE1D5F3}');
+//                          cs.pid := 5;
+                          if pc.GetDetailsEx(pidChild, SHCOLUMNID(PKEY_AppUserModel_ID), @sa) = S_OK then
+                          begin
+                            var re: HRESULT;
+                            try
+                              re := (pc.GetDetailsEx(pidChild, SHCOLUMNID(PKEY_Link_Arguments), @flnk));
+                              re := (pc.GetDetailsEx(pidChild, SHCOLUMNID(PKEY_Link_TargetParsingPath), @lnk));
+//                              SHGetPathFromIDList(pidAbsolute, fPath);
+                            except
+                              lnk := '';
+                            end;
+
+                            if IsValidAppPackageName(sa)
+                            or (Pos('!Setti', sa) > 0)
+                            then
+                            begin
+                              SHGetFileInfo(LPCTSTR(pidAbsolute), 0, fileInfo, SizeOf(fileInfo),
+                                SHGFI_PIDL or SHGFI_DISPLAYNAME or SHGFI_ICON or SHGFI_SYSICONINDEX {or SHGFI_SHELLICONSIZE} or SHGFI_LARGEICON);
+
+//                              pc.GetUIObjectOf(0, )
+//                              var c:uint32;
+//                              pc.GetAttributesOf(1,pidChild,c);
+                              CoTaskMemFree(pidAbsolute);
+
+                              // Get WSA (wsaclient.exe) info
+                              if Pos('!Sett', sa) > 0 then
+                              begin
+                                FDisplayName := fileInfo.szDisplayName;
+                                FAppUserModelId := sa;
+                              end
+                              // and APK installed ones info
+                              else if LowerCase(lnk).Contains('wsaclient.exe') then//if IsWsaClientLnkTarget(sa) then
+                              begin
+                                Item := TStrings.Create;
+                                try
+
+                                finally
+                                  Item.Free;
+                                end;
+                              end;
+
+                              var icon := TIcon.Create;
+                              try
+                                icon.Handle := fileInfo.hIcon;
+
+                              finally
+                                icon.Free;
+                              end;
+                            end;
+
+
+                          end;
+
+                        end;
+                      end;
+                    end;
+          end;
+        end;
+      end;
+    end;
+  end;
+
+  CoUninitialize;
 end;
 
 { TAPKList }
